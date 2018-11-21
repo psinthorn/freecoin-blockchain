984bb01b835466508d5d47406b7657ff08420d23
ecosyn <psinthorn@gmail.com>
2018-11-22 00:45:37 +0700

    1st init

--- constructors/Car.js
+++
@@ -1,9 +0,0 @@
-// Car Constructor
-function Car(brand, model, year) {
-    this.brand = brand;
-    this.model = model;
-    this.year = year;
-}
-
-// Module Exports for use elsewhere
-module.exports.Car = Car
